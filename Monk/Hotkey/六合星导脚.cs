0b9f6140178c5e95e93f86d59c2096ba0ab7be4f
xiseliuli <xiseliuli@gmail.com>
2025-09-03 12:56:52 +0800

    feat: 更新`六合星导脚`逻辑，优化技能操作与显示
    - 调整技能图标渲染逻辑，添加对快捷键显示的支持。
    - 优化技能处理流程，增强高优先级GCD插槽管理。
    - 引入额外扩展接口，提升代码模块化和功能可维护性。

--- Monk/Hotkey/六合星导脚.cs
+++ Monk/Hotkey/六合星导脚.cs
@@ -1,7 +1,14 @@
 ﻿using System.Numerics;
+using AEAssist;
+using AEAssist.CombatRoutine;
 using AEAssist.CombatRoutine.Module;
 using AEAssist.CombatRoutine.View.JobView;
+using AEAssist.Extension;
 using AEAssist.Helper;
+using AEAssist.MemoryApi;
+using Dalamud.Game.ClientState.Objects.Types;
+using Dalamud.Interface.Textures.TextureWraps;
+using ImGuiNET;
 using Xise.Monk.SlotResolver.Data;
 
 namespace Xise.Monk.Hotkey;
@@ -10,10 +17,16 @@ public class 六合星导脚 : IHotkeyResolver
 {
     public void Draw(Vector2 size)
     {
+        Vector2 vector2 = size * 0.8f;
+        ImGui.SetCursorPos(size * 0.1f);
+        IDalamudTextureWrap idalamudTextureWrap;
+        Core.Resolve<MemApiIcon>().GetActionTexture(Spells.六合星导脚, out idalamudTextureWrap);
+        ImGui.Image(idalamudTextureWrap.ImGuiHandle, vector2);
     }
 
     public void DrawExternal(Vector2 size, bool isActive)
     {
+        SpellHelper.DrawSpellInfo(new Spell(Spells.六合星导脚, GameObjectExtension.GetCurrTarget(Core.Me)), size, isActive);
     }
 
     public int Check()
@@ -23,6 +36,10 @@ public class 六合星导脚 : IHotkeyResolver
 
     public void Run()
     {
-        AI.Instance.BattleData.NextSlot.Add((Spells.六合星导脚.GetSpell()));
+        Spell spell = new Spell(Spells.六合星导脚, GameObjectExtension.GetCurrTarget(Core.Me))
+        {
+            DontUseGcdOpt = true
+        };
+        AI.Instance.BattleData.HighPrioritySlots_GCD.Enqueue(new Slot().Add(spell));
     }
 }
