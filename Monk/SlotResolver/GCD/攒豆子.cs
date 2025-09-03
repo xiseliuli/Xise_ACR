64b193b921c94fde7e158dafef9d3d5295c261a3
xiseliuli <xiseliuli@gmail.com>
2025-09-03 13:01:59 +0800

    feat: 添加`攒豆子`技能解析器
    - 引入`攒豆子`解析器至GCD分类，用于低等级技能处理。
    - 优化技能槽位构建逻辑，增强模块化设计。

---
+++ Monk/SlotResolver/GCD/攒豆子.cs
@@ -0,0 +1,20 @@
+﻿using AEAssist;
+using AEAssist.CombatRoutine.Module;
+using AEAssist.Helper;
+using Xise.Monk.SlotResolver.Data;
+
+namespace Xise.Monk.SlotResolver.GCD;
+
+public class 攒豆子 : ISlotResolver
+{
+    public int Check()
+    {
+        if (Core.Me.Level < 15) return -1;
+        return 0;
+    }
+
+    public void Build(Slot slot)
+    {
+        slot.Add(Spells.斗气adaptive.GetSpell());
+    }
+}
