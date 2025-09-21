ae18b03dd8dc6999d7d42061d23c6e1fc4ed7ea9
xiseliuli <xiseliuli@gmail.com>
2025-09-21 10:06:32 +0800

    fix: 修复`六合星导脚`图标渲染逻辑，更新接口调用

--- Monk/Hotkey/六合星导脚.cs
+++ Monk/Hotkey/六合星导脚.cs
@@ -6,22 +6,22 @@ using AEAssist.CombatRoutine.View.JobView;
 using AEAssist.Extension;
 using AEAssist.Helper;
 using AEAssist.MemoryApi;
+using Dalamud.Bindings.ImGui;
 using Dalamud.Game.ClientState.Objects.Types;
 using Dalamud.Interface.Textures.TextureWraps;
-using ImGuiNET;
 using Xise.Monk.SlotResolver.Data;
 
 namespace Xise.Monk.Hotkey;
 
 public class 六合星导脚 : IHotkeyResolver
 {
-    public void Draw(Vector2 size)
+    public void Draw(Vector2 size)  
     {
         Vector2 vector2 = size * 0.8f;
         ImGui.SetCursorPos(size * 0.1f);
         IDalamudTextureWrap idalamudTextureWrap;
         Core.Resolve<MemApiIcon>().GetActionTexture(Spells.六合星导脚, out idalamudTextureWrap);
-        ImGui.Image(idalamudTextureWrap.ImGuiHandle, vector2);
+        ImGui.Image(idalamudTextureWrap.Handle, vector2);
     }
 
     public void DrawExternal(Vector2 size, bool isActive)
